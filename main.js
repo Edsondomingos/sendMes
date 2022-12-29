15ee612ccebf7b33cda5735706d0824048beb46b
Edson Domingos <edomingoswork@gmail.com>
2022-12-29 18:53:01 -0300

    implementação do localStorage

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const form = document.querySelector('form')
-const data = document.querySelector('[type-date]')
-const mensagem = document.querySelector('[type-msg]')
-const nome = document.querySelector('[type-nome]')
+const data = document.querySelector('[data-type-date]')
+const mensagem = document.querySelector('[data-type-msg]')
+const nome = document.querySelector('[data-type-nome]')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    window.location.href = data.value
-    // let msg = document.createElement()
-    let msg = `
-        <h1>${data.value}</h1>
-        <p>${mensagem.value}</p>
-        <p>${nome.value}</p>
-    `
+    form.style.display = 'none'
+    let info = {
+        data: data.value,
+        mensagem: mensagem.value,
+        nome: nome.value
+    }
+    
+    localStorage.setItem('dados',JSON.stringify(info))
+    window.location.href = 'mes.html?' + data.value + '?' + mensagem.value + '?' + nome.value
+    // window.location.href = 'mes.html'
+    // console.log(mensagem.value)
+    // let msg = document.createElement('main')
+    // let conteudo = `
+    //     <h1>${data.value}</h1>
+    //     <p>${mensagem.value}</p>
+    //     <p>${nome.value}</p>
+    // `
+    // msg.innerHTML = conteudo
+    // document.body.appendChild(msg)
 })
